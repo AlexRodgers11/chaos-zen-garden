67165f2a1b82666cdfdc5be07e9cc0e03c2b9416
Alex Rodgers <skyewulff@outlook.com>
2021-07-14 22:55:10 -0400

    get basic animation working in Snake using useEffect, useRef, and setTimeout

--- ocd-zen-garden/src/Snake.js
+++ ocd-zen-garden/src/Snake.js
@@ -1,31 +1,79 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import useToggle from './hooks/useToggle';
-import SnakeBox from './SnakeBox';
-
-//clicking scatter button has to give each one a random offset
-////push objects into boxArr that have randomly assigned offsets and 
-//clicking organize has to, one at at time, correct the offset
-
 
 function Snake(props) {
-    const [isOrganized, toggleIsOrganized] = useToggle(true);
-    const numBoxes = props.num || 10;
-    const boxArr = [];
-    for(let k = 1; k <= numBoxes; k++){
-        let randomNum = 275 + Math.floor(Math.random() * 50);
-        boxArr.push({
-            offset: `${randomNum}px`,
-            delay: k * 3
+    const [isOrganized, toggleIsOrganized] = useToggle(false);
+    const [nextIndex, setNextIndex] = useState(1);
+    const [boxes, setBoxes] = useState([{id: 1, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 2, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 3, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 4, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 5, margin: `${325 + Math.floor(Math.random() * 50)}px`}]);
+
+    const firstUpdate = useRef(true);
+    useEffect(()=>{
+        if(!firstUpdate.current) {
+            if(nextIndex < boxes.length){
+                setTimeout(()=>{
+                    organizeBoxes(nextIndex);
+                }, 1000);
+            }
+        } else {firstUpdate.current = false}     
+    }, [nextIndex, boxes])
+
+    const organizeBoxes = (idx) => {
+        let newBoxes = boxes.map(box => {
+            if(box.id === boxes[idx].id){
+                return {...box, margin: '350px'}
+            } else {
+                return box
+            }
         });
+        setBoxes(newBoxes);
+        setNextIndex(idx + 1);
+        if(idx + 1 === boxes.length) setTimeout(() => toggleIsOrganized(), 1000)
+    }
+
+
+    // const organizeBoxes = (idx) => {
+    //     let newBoxes = boxes.map(box => {
+    //         if(box.id === boxes[idx].id){
+    //             return {...box, margin: '350px'}
+    //         } else {
+    //             return box
+    //         }
+    //     });
+    //     setBoxes(newBoxes);
+    //     setNextIndex(idx + 1);
+    //     toggleIsOrganized()
+    // }
+
+    // const organizeBoxes = () => {
+    //     for(let k = 0; k < boxes.length; k++){
+    //         let newBoxes = boxes.map(box => {
+    //             if(box.id === boxes[k].id){
+    //                 return {...box, margin: '350px'}
+    //             } else {
+    //                 return box
+    //             }
+    //         })
+    //         setTimeout(() => setBoxes(() => newBoxes), 1000);
+    //     };
+    //     toggleIsOrganized()
+    // }
+
+    const scatterBoxes = () => {
+        let newBoxes = boxes.map(box => {
+            let randomNum = `${325 + Math.floor(Math.random() * 50)}px`;
+            return {...box, margin: randomNum};
+        })
+        setBoxes(newBoxes);
+        toggleIsOrganized();
     }
 
     return (
         <div style={{width: '750px'}}>
             <p>Snake Test</p>
-            {boxArr.map(idx => (
-                <SnakeBox delay={idx.delay} isOrganized={isOrganized} margin={idx.offset}/>
+            {boxes.map(box => (
+                <div style={{boxSizing: 'border-box', border: '1.5px solid black', width: '50px', height: '50px', padding: 0, margin: `0 ${box.margin}`}}></div>
             ))}
-            <button onClick={toggleIsOrganized}>{isOrganized ? 'Scatter' : 'Organize'}</button>
+            <button onClick={isOrganized ? scatterBoxes : () => organizeBoxes(0)}>{isOrganized ? 'Scatter' : 'Organize'}</button>
         </div>
     )
 }
