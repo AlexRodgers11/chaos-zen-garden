a22ccbb0110393ea636efda4941a852c41eeb14f
Alex Rodgers <skyewulff@outlook.com>
2021-07-16 13:46:29 -0400

    add basic styling to dots to shape them

--- ocd-zen-garden/src/Dots.js
+++ ocd-zen-garden/src/Dots.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import useToggle from './hooks/useToggle';
 
 function Dots() {
-    const [isOrganized, setIsOrganized] = useToggle(true);
+    const [isOrganized, setIsOrganized] = useToggle(false);
     const [nextIndex, setNextIndex] = useState(1);
     const [dots, setDots] = useState([{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7}, {id: 8}, {id: 9}, {id: 10}, {id: 11}, {id: 12}, {id: 13}, {id: 14}, {id: 15}, {id: 16}]);
 
@@ -19,42 +19,26 @@ function Dots() {
         console.log(dotLines);
         return dotLines;
     }
-    
-    
-    
-    
-    // const displayDots = () => {
-    //     let dotLines = []
-    //     let newLine = []
-    //     // let idx = 0;
-    //     while (dotLines.length < (dots.length / 4)) {
-    //         // for(let k = idx; k < dots.length; k++){
-    //         for(let k = 0; k < dots.length; k++){
-    //             if(newLine.length === 4){
-    //                 dotLines.push(newLine);
-    //                 newLine = []
-    //             }
-    //             newLine.push(dots[k]);
-    //             // idx++;
-    //         }
-    //         console.log(dotLines.length)
-    //     }
-    //     console.log(dotLines);
-    //     return dotLines;
-    // }
+    const organizeDots = () => {
+        
+    }
 
+    const scatterDots = () => {
+
+    }
 
     return (
         <div>
-            <p>Dots Testttt</p>
-            <div style={{width: '30%'}}>
+            <p>Dots Test</p>
+            <div style={{width: '45%', margin: '0 auto'}}>
                 {displayDots().map(dotLine => {
-                    return <p>
+                    return <p style={{marginBlockEnd: 0, marginBlockStart: 0, marginBottom: '0 2.5px', padding: 0}}>
                         {dotLine.map(dot => {
-                            return <span>Dot: {dot.id}</span>
+                            return <span style={{display: 'inline-block', border: '1px solid black', borderRadius: '50%', width: '20px', height: '20px', margin: '30px', marginBottom: '30px'}}></span>
                         })}
                     </p>
                 })}
+                <button onClick={isOrganized ? scatterDots : () => scatterDots(0)}>{isOrganized ? 'Scatter' : 'Organize'}</button>
             </div>
         </div>
     )
