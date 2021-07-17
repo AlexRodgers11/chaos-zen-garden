5a3e76534d56dec09863fdd0e33974e07c11df41
Alex Rodgers <skyewulff@outlook.com>
2021-07-17 10:34:45 -0400

    make Snake box and Dots dots responsive to changing window sizes by styling with percentages of the width passed down from garden

--- ocd-zen-garden/src/Dots.js
+++ ocd-zen-garden/src/Dots.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import useToggle from './hooks/useToggle';
 
-function Dots() {
+function Dots(props) {
     const [isOrganized, setIsOrganized] = useToggle(false);
     const [nextIndex, setNextIndex] = useState(1);
     const [dots, setDots] = useState([{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7}, {id: 8}, {id: 9}, {id: 10}, {id: 11}, {id: 12}, {id: 13}, {id: 14}, {id: 15}, {id: 16}]);
@@ -30,11 +30,12 @@ function Dots() {
     return (
         <div>
             <p>Dots Test</p>
-            <div style={{width: '45%', margin: '0 auto'}}>
+            {/* <div style={{width: '45%', margin: '0 auto'}}> */}
+            <div style={{margin: '0 auto'}}>
                 {displayDots().map(dotLine => {
-                    return <p style={{marginBlockEnd: 0, marginBlockStart: 0, marginBottom: '0 2.5px', padding: 0}}>
+                    return <p style={{marginBlockEnd: 0, marginBlockStart: 0, padding: 0}}>
                         {dotLine.map(dot => {
-                            return <span style={{display: 'inline-block', border: '1px solid black', borderRadius: '50%', width: '20px', height: '20px', margin: '30px', marginBottom: '30px'}}></span>
+                            return <span style={{display: 'inline-block', border: '1px solid black', borderRadius: '50%', width: `${props.width * .33 * .035}px`, height: `${props.width * .33 * .035}px`, margin: `${props.width * .33 * .035}px`, marginBottom: `${props.width * .33 * .03}px`}}></span>
                         })}
                     </p>
                 })}
--- ocd-zen-garden/src/Snake.js
+++ ocd-zen-garden/src/Snake.js
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import useToggle from './hooks/useToggle';
 
-function Snake() {
+function Snake(props) {
     const [isOrganized, toggleIsOrganized] = useToggle(false);
     const [nextIndex, setNextIndex] = useState(1);
-    const [boxes, setBoxes] = useState([{id: 1, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 2, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 3, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 4, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 5, margin: `${325 + Math.floor(Math.random() * 50)}px`}]);
+    // const [boxes, setBoxes] = useState([{id: 1, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 2, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 3, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 4, margin: `${325 + Math.floor(Math.random() * 50)}px`}, {id: 5, margin: `${325 + Math.floor(Math.random() * 50)}px`}]);
+    const [boxes, setBoxes] = useState([{id: 1, margin: `${props.width * .33 * .5 + Math.floor(Math.random() * props.width * .33 * .085)}px`}, {id: 2, margin: `${props.width * .33 * .5 + Math.floor(Math.random() * props.width * .33 * .085)}px`}, {id: 3, margin: `${props.width * .33 * .5 + Math.floor(Math.random() * props.width * .33 * .085)}px`}, {id: 4, margin: `${props.width * .33 * .5 + Math.floor(Math.random() * props.width * .33 * .085)}px`}, {id: 5, margin: `${props.width * .33 * .5 + Math.floor(Math.random() * props.width * .33 * .085)}px`}]);
 
     const firstUpdate = useRef(true);
     useEffect(()=>{
@@ -21,7 +22,8 @@ function Snake() {
         let newBoxes;
         if(idx + 1 === boxes.length){
             newBoxes = boxes.map(box => {
-                return {...box, margin: '350px'} 
+                // return {...box, margin: '350px'} 
+                return {...box, margin: `${props.width * .33 * .5}px`} 
             });
         } else {
             newBoxes = boxes.map(box => {
@@ -39,7 +41,8 @@ function Snake() {
 
     const scatterBoxes = () => {
         let newBoxes = boxes.map(box => {
-            let randomNum = `${325 + Math.floor(Math.random() * 50)}px`;
+            // let randomNum = `${325 + Math.floor(Math.random() * 50)}px`;
+            let randomNum = `${props.width * .33 * .5 + Math.floor(Math.random() * props.width * .33 * .085)}px`;
             return {...box, margin: randomNum};
         })
         setBoxes(newBoxes);
@@ -47,10 +50,10 @@ function Snake() {
     }
 
     return (
-        <div style={{width: '750px'}}>
+        <div>
             <p>Snake Test</p>
             {boxes.map(box => (
-                <div style={{boxSizing: 'border-box', border: '1.5px solid black', width: '50px', height: '50px', padding: 0, margin: `0 ${box.margin}`}}></div>
+                <div style={{boxSizing: 'border-box', border: '1.5px solid black', width: `${props.width * .33 * .085}px`, height: `${props.width * .33 * .085}px`, padding: 0, margin: `0 ${box.margin}`}}></div>
             ))}
             <button onClick={isOrganized ? scatterBoxes : () => organizeBoxes(0)}>{isOrganized ? 'Scatter' : 'Organize'}</button>
         </div>
