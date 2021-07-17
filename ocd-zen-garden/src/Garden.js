9a3a7868417e1ce0e555170dcef9b8fd92c63860
Alex Rodgers <skyewulff@outlook.com>
2021-07-17 10:33:54 -0400

    get width in Garden using useCurrentWidth hook and pass it down to Snake and Dots components

--- ocd-zen-garden/src/Garden.js
+++ ocd-zen-garden/src/Garden.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import useCurrentWidth from './hooks/useCurrentWidth';
 import Snake from './Snake';
 import Dots from './Dots';
+import './Garden.css';
 
 function Garden(){
+    let width = useCurrentWidth();
     return(
         <div className="Garden">
-            <Snake />
-            <Dots />
+            <Snake width={width} className="Snake"/>
+            <Dots width={width} className="Dots"/>
+            <div className="Temp">Temp fake garden piece with width: {width}</div>
         </div>
     )
 }
