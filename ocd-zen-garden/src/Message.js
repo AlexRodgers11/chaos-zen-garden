d8eaa1f9d4afd45383705ee446a5de43d8bad9c9
Alex Rodgers <skyewulff@outlook.com>
2021-07-20 14:10:52 -0400

    finish basic logic for Message component to function properly

--- ocd-zen-garden/src/Message.js
+++ ocd-zen-garden/src/Message.js
@@ -3,20 +3,51 @@ import useToggle from './hooks/useToggle';
 
 function Message(props){
     const [isOrganized, toggleIsOrganized] = useToggle(false);
-    const [nextIndex, setNextIndex] = useState(1);
+    const [nextIndex, setNextIndex] = useState(0);
     // const [letters, setLetters] = useState(['s', 'e', 'r', 'e', 'n', 'i', 't', 'y']);
     const generateTilt = () => {
         let x = Math.floor(Math.random() * 2) === 0 ? -1 : 1;
-        let tilt = x * Math.random() * 50;
+        let tilt = x * Math.random() * 55;
         return `${tilt}deg`
         
     }
+
     const [letters, setLetters] = useState([{id: 1, letter: 's', tilt: generateTilt()}, {id: 2, letter: 'e', tilt: generateTilt()}, {id: 3, letter: 'r', tilt: generateTilt()}, {id: 4, letter: 'e', tilt: generateTilt()}, {id: 5, letter: 'n', tilt: generateTilt()}, {id: 6, letter: 'i', tilt: generateTilt()}, {id: 7, letter: 't', tilt: generateTilt()}, {id: 8, letter: 'y', tilt: generateTilt()}]);
 
+    let firstUpdate = useRef(true);
+    useEffect(() => {
+        if(!firstUpdate.current) {
+            if(nextIndex < letters.length){
+                setTimeout(() => {
+                    straightenLetters(nextIndex);
+                }, 500);
+            }
+        } else {
+            firstUpdate.current = false;
+        }
+    }, [letters]);
+    
     const straightenLetters = (idx) => {
+        let newLetters = letters.map(letter => {
+            if(letter.id === letters[idx].id) {
+                return {...letter, tilt: `0deg`}
+            } else return letter;
+        });
+        setLetters(newLetters);
+        setNextIndex(idx + 1);
+        if(idx + 1 === letters.length) {
+            setTimeout(() => {
+                toggleIsOrganized();
+            }, 1000)
+        }
     }
 
     const unalignLetters = () => {
+        let newLetters = letters.map(letter => {
+            return {...letter, tilt: generateTilt()};
+        });
+        setLetters(newLetters);
+        toggleIsOrganized();
     }
 
     return (
