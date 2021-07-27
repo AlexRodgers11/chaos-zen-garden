1c8074f02ef0a7b4f790e4173f94005391f80eb6
Alex Rodgers <skyewulff@outlook.com>
2021-07-27 08:47:28 -0400

    get rid of unused colors in Garden component; remove passing down of color object to Snake component

--- ocd-zen-garden/src/Garden.js
+++ ocd-zen-garden/src/Garden.js
@@ -8,20 +8,11 @@ import Message from './Message';
 import Dominoes from './Dominoes';
 import './Garden.css';
 
-const colors = {
-    salmon: '#ff0048',
-    lime: '#44ff00',
-    blue: '#3e0eff',
-    orange: '#ffa30e',
-    pink:  '#ff00fb',
-    yellow: '#ffff00'
-}
-
 function Garden(){
     let width = useCurrentWidth();
     return(
         <div className="Garden">
-            <Snake width={width} colors={colors} className="Snake" />
+            <Snake width={width} className="Snake" />
             <Dots width={width} className="Dots" />
             <BullsEye width={width} id={1} numRings={10} className="BullsEye" orgIndex={11}/>
             <Message width={width} className="Message" />
