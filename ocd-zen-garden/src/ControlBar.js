e4d96cd7b813a430fcea71010ae7f76561145e43
Alex Rodgers <skyewulff@outlook.com>
2021-07-30 22:11:14 -0400

    add input to ControlBar component that conditionally renders if provided a text prop; set up function to pass text changes up to any parent component

--- ocd-zen-garden/src/ControlBar.js
+++ ocd-zen-garden/src/ControlBar.js
@@ -2,18 +2,26 @@ import React, { useState } from 'react';
 
 function ControlBar(props) {
     const [speed, setSpeed] = useState(1000);
-    const handleChange = evt => {
+    const [text, setText] = useState(props.textValue || null);
+    
+    const handleSpeedChange = evt => {
         setSpeed(evt.target.value);
         props.setSpeed(evt.target.value);
     }
 
+    const handleChangeText = evt => {
+        setText(evt.target.value);
+        props.changeText(evt.target.value);
+    }
+
     return (
         <div>
+            {props.text ? <p><label for="textInput">{props.text}</label><input type="text" onChange={handleChangeText} value={text}/></p> : null}
             <span>
                 <button>Add</button>
                 <button>Change Colors</button>
                 {/* <button>Change Speed</button> */}
-                <select value={speed} onChange={handleChange}>
+                <select value={speed} onChange={handleSpeedChange}>
                     <option value={4000}>.25x</option>
                     <option value={2000}>.5x</option>
                     <option value={1000}>1x</option>
