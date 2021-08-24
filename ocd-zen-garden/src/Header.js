4ca731552206182b0bd1a3c22b76c9feaee3dcd6
Alex Rodgers <skyewulff@outlook.com>
2021-08-23 22:20:35 -0400

    add content and styling to header

--- ocd-zen-garden/src/Header.js
+++ ocd-zen-garden/src/Header.js
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
+import { getColor } from './utils';
 
 function Header(){
     return(
-        <div>
-            Header Test
+        <div style={{display: 'flex', alignItems: 'center', position: 'fixed', width: '100%', height:'45px', backgroundColor: '#303030', fontFamily: 'Arial Narrow, sans-serif', fontWeight: '600', fontSize: '1.85em', borderBottom: '1.7px solid #000', zIndex: 2}}>
+            <div>
+                <span style={{marginLeft: '.5em', color: getColor(1, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>O</span>
+                <span style={{color: getColor(2, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>C</span>
+                <span style={{marginRight: '.25em', color: getColor(3, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>D</span>
+                <span style={{color: getColor(4, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>Z</span>
+                <span style={{color: getColor(5, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>E</span>
+                <span style={{marginRight: '.25em', color: getColor(6, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>N</span>
+                <span style={{color: getColor(7, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>G</span>
+                <span style={{color: getColor(8, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>A</span>
+                <span style={{color: getColor(9, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>R</span>
+                <span style={{color: getColor(10, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>D</span>
+                <span style={{color: getColor(11, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>E</span>
+                <span style={{color: getColor(12, 'Carnival'), textShadow:`-1.425px -1.425px 0 #000,1.425px -1.425px 0 #000,-1.425px 1.425px 0 #000,1.425px 1.425px 0 #000`}}>N</span>
+            </div>
         </div>
     )
 }
