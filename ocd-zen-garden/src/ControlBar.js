5ec0833c118ea07aa02f00d3ec09e878b1acf7a3
Alex Rodgers <skyewulff@outlook.com>
2021-08-25 18:53:08 -0400

    add button to ControlBar to show/hide controls other than organize and expand to full window; move organize button above the rest; push show/hide button and full window button to opposite ends of bottom row

--- ocd-zen-garden/src/ControlBar.js
+++ ocd-zen-garden/src/ControlBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import useToggle from './hooks/useToggle';
 import { palettes, sounds } from './utils';
 
 function ControlBar(props) {
@@ -7,6 +8,7 @@ function ControlBar(props) {
     const [sound, setSound] = useState(props.soundValue || null);
     const [number, setNumber] = useState(props.number);
     const [palette, setPalette] = useState(props.palette || 'baseColors');
+    const [hidden, toggleHidden] = useToggle(true);
      
     const handleSpeedChange = evt => {
         setSpeed(evt.target.value);
@@ -42,48 +44,73 @@ function ControlBar(props) {
     }
 
     return (
+        <>
         <div>
-            {props.text ? <p><label htmlFor="textInput">{props.text}</label><input type="text" disabled={props.isOrganizing} onChange={handleChangeText} value={text}/></p> : null}
-            <span>
-                <button>Add</button>
-                <select id="palette" value={props.palette} onChange={handlePaletteChange}>
-                    {palettes.map(palette => {
-                        return <option value={palette}>{palette}</option>
-                    })}
-                </select>
-                {/* <label forName="speed">Speed</label> */}
-                <select id="speed" value={speed} onChange={handleSpeedChange}>
-                    <option value={4000}>.25x</option>
-                    <option value={2000}>.5x</option>
-                    <option value={1000}>1x</option>
-                    <option value={800}>1.25x</option>
-                    <option value={500}>2x</option>
-                    <option value={200}>5x</option>
-                </select>
-                {/* <label forName="sound">Sound</label> */}
-                <select id="sound" value={sound} onChange={handleSoundChange}>
-                    {sounds.map(sound => {
-                        return <option value={sound}>{sound}</option>
-                    })}
-                </select>
-                {props.number ? <select value={props.number} onChange={handleNumberChange}>
-                    {displayNumberOptions(props.minNum, props.maxNum).map(num => {
-                        return <option value={num}>{num}</option>
-                    })}
-                </select> : null}
-            </span>
             <span>
                 <button disabled={props.isOrganizing} onClick={props.isOrganized ? props.organizedFunction : props.unorganizedFunction}>{props.isOrganized ? props.unorgButton : props.orgButton}</button>
             </span>
-            {!props.disableFullWindow ? 
-                <span>
-                    <button onClick={props.toggleWindow}>{props.fullWindow ? 'Back to Garden' : 'Full Window'}</button>
-                </span>
-                :
-                null
-            }
-            
         </div>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <div>
+                <button onClick={toggleHidden}>{'->'}</button>
+                <div style={{display: !hidden ? 'inline-block' : 'none'}}>
+                    {props.text ? <p><label htmlFor="textInput">{props.text}</label><input type="text" disabled={props.isOrganizing} onChange={handleChangeText} value={text}/></p> : null}
+                    <span>
+                        <button>Add</button>
+                        <select id="palette" value={props.palette} onChange={handlePaletteChange}>
+                            {palettes.map(palette => {
+                                return <option value={palette}>{palette}</option>
+                            })}
+                        </select>
+                        {/* <label forName="speed">Speed</label> */}
+                        <select id="speed" value={speed} onChange={handleSpeedChange}>
+                            <option value={4000}>.25x</option>
+                            <option value={2000}>.5x</option>
+                            <option value={1000}>1x</option>
+                            <option value={800}>1.25x</option>
+                            <option value={500}>2x</option>
+                            <option value={200}>5x</option>
+                        </select>
+                        {/* <label forName="sound">Sound</label> */}
+                        <select id="sound" value={sound} onChange={handleSoundChange}>
+                            {sounds.map(sound => {
+                                return <option value={sound}>{sound}</option>
+                            })}
+                        </select>
+                        {props.number ? <select value={props.number} onChange={handleNumberChange}>
+                            {displayNumberOptions(props.minNum, props.maxNum).map(num => {
+                                return <option value={num}>{num}</option>
+                            })}
+                        </select> : null}
+                    </span>
+                    {/* <span>
+                        <button disabled={props.isOrganizing} onClick={props.isOrganized ? props.organizedFunction : props.unorganizedFunction}>{props.isOrganized ? props.unorgButton : props.orgButton}</button>
+                    </span>
+                    {!props.disableFullWindow ? 
+                        <span>
+                            <button onClick={props.toggleWindow}>{props.fullWindow ? 'Back to Garden' : 'Full Window'}</button>
+                        </span>
+                        :
+                        null
+                    } */}
+                    
+                </div>
+                
+            </div>
+            <div>
+                {/* <span>
+                    <button disabled={props.isOrganizing} onClick={props.isOrganized ? props.organizedFunction : props.unorganizedFunction}>{props.isOrganized ? props.unorgButton : props.orgButton}</button>
+                </span> */}
+                {!props.disableFullWindow ? 
+                    <span>
+                        <button onClick={props.toggleWindow}>{props.fullWindow ? 'Back to Garden' : 'Full Window'}</button>
+                    </span>
+                    :
+                    null
+                }
+            </div>
+        </div>
+        </>
     )
 }
 
