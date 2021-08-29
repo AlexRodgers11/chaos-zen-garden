f6234f9953d17f58809598ec509ac0b84991e68e
Alex Rodgers <skyewulff@outlook.com>
2021-08-28 23:53:27 -0400

    switch out icon for closing full window; make all dropdowns in ControlBar dropups instead

--- ocd-zen-garden/src/ControlBar.js
+++ ocd-zen-garden/src/ControlBar.js
@@ -7,7 +7,8 @@ import { GiConsoleController, GiHamburgerMenu, GiRabbit, GiTortoise } from 'reac
 import { IoIosColorPalette, IoSpeedometerSharp } from 'react-icons/io/';
 import { GoBell } from 'react-icons/go/';
 import { SiAddthis } from 'react-icons/si';
-import { ImSortNumbericDesc } from 'react-icons/im';
+import { ImSortNumbericDesc, ImShrink2 } from 'react-icons/im';
+
 import './ControlBar.css';
 
 
@@ -18,7 +19,7 @@ function ControlBar(props) {
     const [number, setNumber] = useState(props.number);
     const [palette, setPalette] = useState(props.palette);
     const [hidden, toggleHidden] = useToggle(true);
-    const [showDropdown, setShowDropdown] = useState(
+    const [showPopup, setShowPopup] = useState(
         {
             palette: false,
             speed: false,
@@ -42,7 +43,7 @@ function ControlBar(props) {
     // }
     const handleSpeedChange = speed => {
         console.log('handleSpeedChange ran')
-        handleToggleDropdown('speed');
+        handleTogglePopup('speed');
         setSpeed(speed);
         props.setSpeed(speed);
     }
@@ -53,19 +54,19 @@ function ControlBar(props) {
     }
 
     const handleSoundChange = sound => {
-        handleToggleDropdown('sound')
+        handleTogglePopup('sound')
         setSound(sound);
         props.setSound(sound);
     }
     
     const handleNumberChange = number => {
-        handleToggleDropdown('number');
+        handleTogglePopup('number');
         setNumber(number);
         props.setNumber(number);
     }
 
     const handlePaletteChange = palette => {
-        handleToggleDropdown('palette');
+        handleTogglePopup('palette');
         setPalette(palette);
         props.setPalette(palette);
     }
@@ -88,8 +89,8 @@ function ControlBar(props) {
     //     // setShowDropdown({[evt.target.id]: true});
     // }
 
-    const handleToggleDropdown = group => {
-        setShowDropdown({...showDropdown, [group]: !showDropdown[group]});
+    const handleTogglePopup = group => {
+        setShowPopup({...showPopup, [group]: !showPopup[group]});
     }
 
     return (
@@ -107,10 +108,11 @@ function ControlBar(props) {
                 </button>
                 <div style={{display: !hidden ? 'inline-block' : 'none'}}>
                     <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} ><SiAddthis size='1.5em' /></button>
-                    <div onMouseLeave={showDropdown.palette ? () => handleToggleDropdown('palette') : null} className={`ControlBar_dropdown ${showDropdown.palette ? 'ControlBar_dropdown-active' : ''}`}>
-                        <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} id="speed" onClick={() => handleToggleDropdown('palette')}><IoIosColorPalette size='1.5em' /></button>
+                    <div onMouseLeave={showPopup.palette ? () => handleTogglePopup('palette') : null} className={`ControlBar_popup ${showPopup.palette ? 'ControlBar_popup-active' : ''}`}>
+                        <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} id="speed" onClick={() => handleTogglePopup('palette')}><IoIosColorPalette size='1.5em' /></button>
                         {/* <div onMouseLeave={() => handleToggleDropdown('palette')} className='dropdown-content'> */}
-                        <div className='ControlBar_dropdown-content'>
+                        {/* <div className={`ControlBar_dropdown-content ${props.fullWindow ? 'ControlBar_dropup-content' : ''}`}> */}
+                        <div className='ControlBar_popup-content'>
                             {palettes.map(palette => {
                                 return <p onClick={() => handlePaletteChange(palette)}>{palette}</p>
                             })}
@@ -122,10 +124,10 @@ function ControlBar(props) {
                             return <option value={palette}>{palette}</option>
                         })}
                     </select> */}
-                    <div onMouseLeave={showDropdown.speed ? () => handleToggleDropdown('speed') : null} className={`ControlBar_dropdown ${showDropdown.speed ? 'ControlBar_dropdown-active' : ''}`}>
-                        <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} id="speed" onClick={() => handleToggleDropdown('speed')}><GiTortoise size='1.5em' /><GiRabbit size='1.5em' /></button>
+                    <div onMouseLeave={showPopup.speed ? () => handleTogglePopup('speed') : null} className={`ControlBar_popup ${showPopup.speed ? 'ControlBar_popup-active' : ''}`}>
+                        <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} id="speed" onClick={() => handleTogglePopup('speed')}><GiTortoise size='1.5em' /><GiRabbit size='1.5em' /></button>
                         {/* <div onMouseLeave={() => handleToggleDropdown('speed')} className='dropdown-content'> */}
-                        <div className='ControlBar_dropdown-content'>
+                        <div className='ControlBar_popup-content'>
                             <p onClick={() => handleSpeedChange(4000)}>.25x</p>
                             <p onClick={() => handleSpeedChange(2000)}>.5x</p>
                             <p onClick={() => handleSpeedChange(1000)}>1x</p>
@@ -135,9 +137,9 @@ function ControlBar(props) {
                         </div>
                     </div>
                     
-                    <div onMouseLeave={showDropdown.sound ? () => handleToggleDropdown('sound') : null} className={`ControlBar_dropdown ${showDropdown.sound ? 'ControlBar_dropdown-active' : ''}`}>
-                        <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} id="sound" onClick={() => handleToggleDropdown('sound')}><GoBell size='1.5em' /></button>
-                        <div className='ControlBar_dropdown-content'>
+                    <div onMouseLeave={showPopup.sound ? () => handleTogglePopup('sound') : null} className={`ControlBar_popup ${showPopup.sound ? 'ControlBar_popup-active' : ''}`}>
+                        <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} id="sound" onClick={() => handleTogglePopup('sound')}><GoBell size='1.5em' /></button>
+                        <div className='ControlBar_popup-content'>
                             {sounds.map(sound => {
                                 return <p onClick={() => handleSoundChange(sound)} >{sound}</p>
                             })}
@@ -151,9 +153,10 @@ function ControlBar(props) {
                     </select> */}
 
                     {props.number ? 
-                        <div onMouseLeave={showDropdown.number ? () => handleToggleDropdown('number') : null} className={`ControlBar_dropdown ${showDropdown.number ? 'ControlBar_dropdown-active' : ''}`}>
-                            <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} id="number" onClick={() => handleToggleDropdown('number')}><ImSortNumbericDesc size='1.5em' /></button>
-                            <div className='ControlBar_dropdown-content'>
+                        <div onMouseLeave={showPopup.number ? () => handleTogglePopup('number') : null} className={`ControlBar_popup ${showPopup.number ? 'ControlBar_popup-active' : ''}`}>
+                            <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} id="number" onClick={() => handleTogglePopup('number')}><ImSortNumbericDesc size='1.5em' /></button>
+                            <div className='ControlBar_popup-content'>
+                            {/* <div className='ControlBar_dropdown-content'> */}
                                 {displayNumberOptions(props.minNum, props.maxNum).map(num => {
                                     return <p onClick={() => handleNumberChange(num)}>{num}</p>
                                 })}
@@ -177,7 +180,7 @@ function ControlBar(props) {
 
                 {!props.disableFullWindow ? 
                     <span>
-                        <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} onClick={props.toggleWindow}>{props.fullWindow ? <GrContract size='1.5em' /> : <FaExpandAlt size='1.5em' />}</button>
+                        <button style={{color: getColor(1, palette), backgroundColor: '#303030', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} onClick={props.toggleWindow}>{props.fullWindow ? <ImShrink2 size='1.5em' /> : <FaExpandAlt size='1.5em' />}</button>
                     </span>
                     :
                     null
