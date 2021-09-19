bbe05a883c9c4eaffc6f36011985eb68ab9633cf
Alex Rodgers <skyewulff@outlook.com>
2021-09-18 21:46:35 -0400

    refactor cross styling in Crosshair component to make rings line up better

--- ocd-zen-garden/src/Crosshair.js
+++ ocd-zen-garden/src/Crosshair.js
@@ -159,7 +159,7 @@ function Crosshair(props) {
                     transform: `rotate(${rings[id - 1].rotation}deg)`,
                     height: `${ringSize}px`, 
                     width: `${ringSize}px`}}>
-                        {/* <div style={{position: 'absolute', zIndex: `${id}`, height: `${ringSize}px`, width: `${ringSize}px`}}>
+                        <div style={{position: 'absolute', zIndex: `${id}`, height: `${ringSize}px`, width: `${ringSize}px`}}>
                             <div style={{position: 'absolute', width: '100%', height: '50%', zIndex: `${id}`}}>
                                 <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderBottom: '1px solid black', borderRight: '1px solid black'}}></div>
                                 <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderBottom: '1px solid black', borderLeft: '1px solid black'}}></div>
@@ -168,11 +168,11 @@ function Crosshair(props) {
                                 <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderTop: '1px solid black', borderRight: '1px solid black'}}></div>
                                 <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderTop: '1px solid black', borderLeft: '1px solid black'}}></div>
                             </div>
-                        </div> */}
-                        <div className="line-container" style={{position: 'absolute', zIndex: `${id}`, height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                            <hr style={{position: 'absolute', height: '100%', borderColor: 'black'}}/>
-                            <hr style={{position: 'absolute', height: '100%', borderColor: 'black', transform: 'rotate(90deg)'}}/>
                         </div>
+                        {/* <div className="line-container" style={{position: 'absolute', zIndex: `${id}`, height: `100%`, width: `100%`, display: 'flex', justifyContent: 'center', alignItems: 'center', right: '.5px', bottom: '.5px'}}>
+                            <hr style={{position: 'absolute', width: '100%', borderColor: 'black', backgroundColor: 'black'}}/>
+                            <hr style={{position: 'absolute', width: '100%', borderColor: 'black', backgroundColor: 'black', transform: 'rotate(90deg)'}}/>
+                        </div> */}
                         {/* <div style={{position: 'absolute', zIndex: `${id}`, height: `${ringSize}px`, width: `${ringSize}px`, border: '1px solid black'}}>
                             <div style={{position: 'absolute', width: '100%', height: '50%', zIndex: `${id}`}}>
                                 <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 2}px`, zIndex: `${id}`, height: `${ringSize * .5 - 2}px`, borderBottom: '1px solid blue', borderRight: '1px solid blue'}}></div>
@@ -186,6 +186,8 @@ function Crosshair(props) {
                         {display(id + 1)}
                     </div>
             )
+        } else {
+            console.log('broke out')
         }
         // return <div style={{
         //     position: 'relative',
