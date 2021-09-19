3919f48a5e72725da57a02e862b8b6a1d0579619
Alex Rodgers <skyewulff@outlook.com>
2021-09-19 16:18:00 -0400

    remove unused code in Crosshair component

--- ocd-zen-garden/src/Crosshair.js
+++ ocd-zen-garden/src/Crosshair.js
@@ -165,110 +165,19 @@ function Crosshair(props) {
                     width: `${ringSize}px`}}>
                         <div style={{position: 'absolute', zIndex: `${id}`, height: `${ringSize}px`, width: `${ringSize}px`}}>
                             <div style={{position: 'absolute', width: '100%', height: '50%', zIndex: `${id}`}}>
-                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderBottom: '1px solid black', borderRight: '1px solid black'}}></div>
-                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderBottom: '1px solid black', borderLeft: '1px solid black'}}></div>
+                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderBottom: `${1/2}px solid black`, borderRight: `${1/2}px solid black`}}></div>
+                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderBottom: `${1/2}px solid black`, borderLeft: `${1/2}px solid black`}}></div>
                             </div>
                             <div style={{position: 'absolute', width: '100%', height: '50%', top: `${ringSize * .5}px`, zIndex: `${id}`}}>
-                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderTop: '1px solid black', borderRight: '1px solid black'}}></div>
-                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderTop: '1px solid black', borderLeft: '1px solid black'}}></div>
+                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderTop: '.1px solid black', borderRight: '.5px solid black'}}></div>
+                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 1}px`, zIndex: `${id}`, height: `${ringSize * .5 - 1}px`, borderTop: '.1px solid black', borderLeft: '.5px solid black'}}></div>
                             </div>
                         </div>
-                        {/* <div className="line-container" style={{position: 'absolute', zIndex: `${id}`, height: `100%`, width: `100%`, display: 'flex', justifyContent: 'center', alignItems: 'center', right: '.5px', bottom: '.5px'}}>
-                            <hr style={{position: 'absolute', width: '100%', borderColor: 'black', backgroundColor: 'black'}}/>
-                            <hr style={{position: 'absolute', width: '100%', borderColor: 'black', backgroundColor: 'black', transform: 'rotate(90deg)'}}/>
-                        </div> */}
-                        {/* <div style={{position: 'absolute', zIndex: `${id}`, height: `${ringSize}px`, width: `${ringSize}px`, border: '1px solid black'}}>
-                            <div style={{position: 'absolute', width: '100%', height: '50%', zIndex: `${id}`}}>
-                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 2}px`, zIndex: `${id}`, height: `${ringSize * .5 - 2}px`, borderBottom: '1px solid blue', borderRight: '1px solid blue'}}></div>
-                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 2}px`, zIndex: `${id}`, height: `${ringSize * .5 - 2}px`, borderBottom: '1px solid green', borderLeft: '1px solid green'}}></div>
-                            </div>
-                            <div style={{position: 'absolute', width: '100%', height: '50%', top: `${ringSize * .5}px`, zIndex: `${id}`}}>
-                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 2}px`, zIndex: `${id}`, height: `${ringSize * .5 - 2}px`, borderTop: '1px solid orange', borderRight: '1px solid orange'}}></div>
-                                <div style={{position: 'relative', display: 'inline-block', width: `${ringSize * .5 - 2}px`, zIndex: `${id}`, height: `${ringSize * .5 - 2}px`, borderTop: '1px solid purple', borderLeft: '1px solid purple'}}></div>
-                            </div>
-                        </div> */}
+
                         {display(id + 1)}
                     </div>
             )
-        } else {
-            console.log('broke out')
-        }
-        // return <div style={{
-        //     position: 'relative',
-        //     margin: '0 auto',
-        //     width: '300px',
-        //     height: '300px',
-        //     border: '1px solid black',
-        //     borderRadius: '50%',
-        //     zIndex: '1',
-        //     backgroundColor: 'green'
-        // }}>
-        //     <div style={{
-                // position: 'absolute',
-                // margin: 'none',
-                // width: '300px',
-                // height: '150px',
-                // zIndex: '1'
-        //     }}>
-        //         <div style={{
-        //             position: 'relative',
-        //             width: '149px',
-        //             height: '149px',
-        //             borderBottom: '1px solid red',
-        //             borderRight: '1px solid red',
-        //             display: 'inline-block',
-        //             zIndex: '1'
-        //         }}></div>
-        //         <div style={{
-        //             position: 'relative',
-        //             width: '148px',
-        //             height: '148px',
-        //             borderLeft: '1px solid red',
-        //             borderBottom: '1px solid red',
-        //             display: 'inline-block',
-        //             zIndex: '1'
-        //         }}></div>
-        //     </div>
-        //     <div style={{
-        //         position: 'absolute',
-        //         top: '50%',
-        //         width: '300px',
-        //         height: '150px',
-        //         zIndex: '1'
-        //     }}>
-        //         <div style={{
-        //             position: 'relative',
-        //             width: '148px',
-        //             height: '148px',
-        //             borderTop: '1px solid red',
-        //             borderRight: '1px solid red',
-        //             display: 'inline-block',
-        //             zIndex: '1'
-        //         }}></div>
-        //         <div style={{
-        //             position: 'relative',
-        //             width: '148px',
-        //             height: '148px',
-        //             borderLeft: '1px solid red',
-        //             borderTop: '1px solid red',
-        //             display: 'inline-block',
-        //             zIndex: '1',
-        //             marginBottom: '0',
-        //         }}></div>
-        //     </div>
-        //     <div style={{
-        //             top: '25px',
-        //             position: 'relative',
-        //             width: '250px',
-        //             height: '250px',
-        //             zIndex: '2',
-        //             backgroundColor: 'blue',
-        //             border: '1px solid blue',
-        //             opacity: '1',
-        //             margin: '0 auto',
-        //             borderRadius: '50%',
-        //         }}></div>
-        // </div>
+        } 
     }
 
     return (
