41da4a89d6364a4d2a47ad34d246dfc8007d7f5d
Alex Rodgers <skyewulff@outlook.com>
2021-10-02 20:45:12 -0400

    fix bug causing slightly different heights of stripes in Dominoes component due to inconsistent rounding

--- ocd-zen-garden/src/Barcode.js
+++ ocd-zen-garden/src/Barcode.js
@@ -144,12 +144,11 @@ function Barcode(props) {
     const [stripes, setStripes] = useState(createStartingStripeArray(numStripes))
     return (
         <div style={{margin: props.fullWindow ? '0 auto' : 0, display: 'flex', justifyContent: 'center', alignItems: 'center', width: `${props.width}px`, height: `${props.width}px`, border: '1px solid black', backgroundColor: getColor('base', colorPalette)}}>
-            {/* <div style={{position: 'relative', width: `${props.width}px`, height: `${.7 * props.width}px`}}> */}
             <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '100%', height: '100%'}}>
                 <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100%', height: '100%'}}>
                     <div>
                         {stripes.map(stripe => {
-                            return <div key={stripe.key} style={{margin: '0 auto', width: `${.4 * props.width}px`, height: `${stripe.height * props.width}px`, backgroundColor: stripe.color, border: `1px solid ${getColor('border', colorPalette)}`}}></div>
+                            return <div key={stripe.key} style={{margin: '0 auto', width: `${.4 * props.width}px`, height: `${Math.floor(stripe.height * props.width)}px`, backgroundColor: stripe.color, border: `1px solid ${getColor('border', colorPalette)}`}}></div>
                         })}
                     </div>
                 </div>
