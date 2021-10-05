AlexRodgers11/chaos-zen-garden

commit ab676468840ca9865636f8ec56853b5bae536205

add button to ControlBar component that controls whether volume is proportional or even; add paremeter to soundPlay in utils to handle the toggling of volume options; add logic to Snake component to use the proportionality toggling feature

ocd-zen-garden/src/ControlBar.js
@@ -10,7 +10,7 @@ import { GoBell } from 'react-icons/go/';
 import { SiAddthis } from 'react-icons/si';
 import { ImSortNumbericDesc, ImShrink2, ImVolumeHigh, ImVolumeLow, ImVolumeMedium, ImVolumeMute } from 'react-icons/im';
 import { BsFileText, BsLockFill } from 'react-icons/bs';
-
+import { RiSoundModuleLine } from 'react-icons/ri';
 import './ControlBar.css';
 
 
@@ -20,6 +20,7 @@ function ControlBar(props) {
     const [sound, setSound] = useState(props.soundValue || null);
     const [number, setNumber] = useState(props.number);
     const [palette, setPalette] = useState(props.palette);
+    const [proportionalVolume, setProportionalVolume] = useState(props.proportionalVolume || null);
     const [hidden, toggleHidden] = useToggle(true);
     const [volume, setVolume] = useState(props.volume);
     const [shape, setShape] = useState(props.shape);
@@ -31,7 +32,8 @@ function ControlBar(props) {
             number: false,
             text: false,
             volume: false,
-            shape: false
+            shape: false,
+            proportionalVolume: false
         }
     )
 
@@ -53,12 +55,8 @@ function ControlBar(props) {
         }
     }, [props.palette]);
      
-    // const handleSpeedChange = evt => {
-    //     setSpeed(evt.target.value);
-    //     props.setSpeed(evt.target.value);
-    // }
+
     const handleSpeedChange = speed => {
-        console.log('handleSpeedChange ran')
         handleTogglePopup('speed');
         setSpeed(speed);
         props.setSpeed(speed);
@@ -88,8 +86,14 @@ function ControlBar(props) {
     }
 
     const handleVolumeChange = evt => {
-        props.changeVolume(evt.target.value)
-        setVolume(evt.target.value)
+        props.changeVolume(evt.target.id)
+        setVolume(evt.target.id)
+    }
+    
+    const handleProportionalVolumeChange = selection => {
+        handleTogglePopup('proportionalVolume');
+        props.changeProportionalVolume(selection)
+        setProportionalVolume(selection)
     }
     
     const handleShapeChange = shape => {
@@ -135,8 +139,8 @@ function ControlBar(props) {
                 </button>
                 <div style={{display: !hidden ? 'inline-block' : 'none'}}>
                     <button disabled={!props.isLoggedIn} style={{color: getColor('aux1', palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '2px'}} ><SiAddthis size='1.5em' /></button>
-                    <div style={{position: 'relative', zIndex: 112}} onMouseLeave={showPopup.palette ? () => handleTogglePopup('palette') : null} className={`ControlBar_popup ${showPopup.palette ? 'ControlBar_popup-active' : ''}`}>
-                        <button disabled={props.isOrganizing} style={{position: 'relative', zIndex: 113, color: getColor('aux1', palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '.15em'}} id="speed" onClick={() => handleTogglePopup('palette')}><IoIosColorPalette size='1.5em' /></button>
+                    <div style={{position: 'relative', zIndex: 114}} onMouseLeave={showPopup.palette ? () => handleTogglePopup('palette') : null} className={`ControlBar_popup ${showPopup.palette ? 'ControlBar_popup-active' : ''}`}>
+                        <button disabled={props.isOrganizing} style={{position: 'relative', zIndex: 115, color: getColor('aux1', palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '.15em'}} id="speed" onClick={() => handleTogglePopup('palette')}><IoIosColorPalette size='1.5em' /></button>
                         {/* <div onMouseLeave={() => handleToggleDropdown('palette')} className='dropdown-content'> */}
                         {/* <div className={`ControlBar_dropdown-content ${props.fullWindow ? 'ControlBar_dropup-content' : ''}`}> */}
                         <div className='ControlBar_popup-content'>
@@ -157,8 +161,8 @@ function ControlBar(props) {
                             return <option value={palette}>{palette}</option>
                         })}
                     </select> */}
-                    <div style={{position: 'relative', zIndex: 110}} onMouseLeave={showPopup.speed ? () => handleTogglePopup('speed') : null} className={`ControlBar_popup ${showPopup.speed ? 'ControlBar_popup-active' : ''}`}>
-                        <button style={{position: 'relative', zIndex: 111, color: getColor('aux1', palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '.15em'}} id="speed" onClick={() => handleTogglePopup('speed')}><GiTortoise size='1.5em' /><GiRabbit size='1.5em' /></button>
+                    <div style={{position: 'relative', zIndex: 112}} onMouseLeave={showPopup.speed ? () => handleTogglePopup('speed') : null} className={`ControlBar_popup ${showPopup.speed ? 'ControlBar_popup-active' : ''}`}>
+                        <button style={{position: 'relative', zIndex: 113, color: getColor('aux1', palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '.15em'}} id="speed" onClick={() => handleTogglePopup('speed')}><GiTortoise size='1.5em' /><GiRabbit size='1.5em' /></button>
                         {/* <div onMouseLeave={() => handleToggleDropdown('speed')} className='dropdown-content'> */}
                         <div className='ControlBar_popup-content'>
                             <div style={{boxShadow: '0px 8px 16px 0px rgba(0,0,0,0.2)'}}>
@@ -179,8 +183,8 @@ function ControlBar(props) {
                         </div>
                     </div>
                     
-                    <div style={{position: 'relative', zIndex: 108}} onMouseLeave={showPopup.sound ? () => handleTogglePopup('sound') : null} className={`ControlBar_popup ${showPopup.sound ? 'ControlBar_popup-active' : ''}`}>
-                        <button style={{position: 'relative', zIndex: 109, color: getColor('aux1', palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '.15em'}} id="sound" onClick={() => handleTogglePopup('sound')}><GoBell size='1.5em' /></button>
+                    <div style={{position: 'relative', zIndex: 110}} onMouseLeave={showPopup.sound ? () => handleTogglePopup('sound') : null} className={`ControlBar_popup ${showPopup.sound ? 'ControlBar_popup-active' : ''}`}>
+                        <button style={{position: 'relative', zIndex: 111, color: getColor('aux1', palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '.15em'}} id="sound" onClick={() => handleTogglePopup('sound')}><GoBell size='1.5em' /></button>
                         <div className='ControlBar_popup-content'>
                             {/* <div style={{paddingBottom: '1.5em'}}>
                             {sounds.map(sound => {
@@ -196,6 +200,26 @@ function ControlBar(props) {
                             <div style={{height: '1.8em', opacity: 0.5, backgroundColor: 'black'}}></div>
                         </div>
                     </div>
+
+                    {props.proportionalVolume ? <div style={{position: 'relative', zIndex: 108}} onMouseLeave={showPopup.proportionalVolume ? () => handleTogglePopup('proportionalVolume') : null} className={`ControlBar_popup ${showPopup.proportionalVolume ? 'ControlBar_popup-active' : ''}`}>
+                        <button style={{position: 'relative', zIndex: 109, color: getColor('aux1', palette), backgroundColor: '#303030', display: 'inline-flex', alignItems: 'center', justifyContent: 'center', padding: '.15em'}} id="proportionalVolume" onClick={() => handleTogglePopup('proportionalVolume')}><RiSoundModuleLine size='1.5em' /></button>
+                        <div className='ControlBar_popup-content'>
+                            <div >
+                            {/* {sounds.map(sound => {
+                                return <p onClick={() => handleSoundChange(sound)} >{sound}</p>
+                            })} */}
+                                <p onClick={() => handleProportionalVolumeChange('even')}>Even Volume</p>
+                                <p onClick={() => handleProportionalVolumeChange('proportional')}>Proportional Volume</p>
+                            </div>
+                            <div style={{boxShadow: '0px 8px 16px 0px rgba(0,0,0,0.2)'}}>
+                                {/* {sounds.map(sound => {
+                                    let key = uuidv4();
+                                    return <p key={key} onClick={() => handleSoundChange(sound)} >{sound}</p>
+                                })} */}
+                            </div>
+                            <div style={{height: '1.8em', opacity: 0.5, backgroundColor: 'black'}}></div>
+                        </div>
+                    </div> : null}
                     
                     <div style={{position: 'relative', zIndex: 106}} onMouseLeave={showPopup.volume ? () => handleTogglePopup('volume') : null} className={`ControlBar_popup ${showPopup.volume ? 'ControlBar_popup-active' : ''}`}>
                         <div style={{display: 'inline-flex', transform: 'rotate(270deg)'}}>

ocd-zen-garden/src/Snake.js
@@ -12,7 +12,8 @@ function Snake(props) {
     const [colorPalette, setColorPalette] = useState(props.palette);
     const [speed, setSpeed] = useState(1000);
     const [sound, setSound] = useState(getSound('Slam'));
-    const [numBoxes, setNumBoxes] = useState(7)
+    const [proportionalVolume, setProportionalVolume] = useState('proportional');
+    const [numBoxes, setNumBoxes] = useState(7);
     const [numSnakes, setNumSnakes] = useState(1);
     const [shape, setShape] = useState('square');
 
@@ -108,7 +109,7 @@ function Snake(props) {
             });
         }
 
-        soundPlay(sound, boxes[idx].volumeMultiplier, props.volume)
+        soundPlay(sound, boxes[idx].volumeMultiplier, props.volume, proportionalVolume);
 
         
         setBoxes(newBoxes);
@@ -143,6 +144,10 @@ function Snake(props) {
     const handleSetColorPalette = palette => {
         colorsDoNotUpdate.current = false;
         setColorPalette(palette);
+    }
+
+    const handleChangeProportionalVolume = selection => {
+        setProportionalVolume(selection);
     }
 
     const handleChangeVolume = volume => {
@@ -180,7 +185,7 @@ function Snake(props) {
                         })}
                     {/* </div> */}
                 </div>
-                <ControlBar toggleWindow={handleToggleWindow} fullWindow={props.fullWindow} disableFullWindow={props.disableFullWindow} setModalContent={props.setModalContent} shape={shape} shapes={['circle', 'square']} changeShape={handleChangeShape} volume={props.volume} changeVolume={handleChangeVolume} palette={colorPalette} setPalette={handleSetColorPalette} minNum={4} maxNum={30} number={numBoxes} setNumber={handleSetNumBoxes} isOrganizing={isOrganizing} isOrganized={isOrganized} setSpeed={handleSetSpeed} setSound={handleSetSound} soundValue='Slam' organizedFunction={scatterBoxes} unorganizedFunction={() => organizeBoxes(0)} unorgButton='Scatter' orgButton='Organize' />
+                <ControlBar toggleWindow={handleToggleWindow} fullWindow={props.fullWindow} disableFullWindow={props.disableFullWindow} setModalContent={props.setModalContent} shape={shape} shapes={['circle', 'square']} changeShape={handleChangeShape} changeProportionalVolume={handleChangeProportionalVolume} proportionalVolume={'proportional'} volume={props.volume} changeVolume={handleChangeVolume} palette={colorPalette} setPalette={handleSetColorPalette} minNum={4} maxNum={30} number={numBoxes} setNumber={handleSetNumBoxes} isOrganizing={isOrganizing} isOrganized={isOrganized} setSpeed={handleSetSpeed} setSound={handleSetSound} soundValue='Slam' organizedFunction={scatterBoxes} unorganizedFunction={() => organizeBoxes(0)} unorgButton='Scatter' orgButton='Organize' />
             </div>
         </div>
     )

ocd-zen-garden/src/utils.js
@@ -423,11 +423,11 @@ const sounds = ['Ding', 'Whoosh', 'Whoop', 'Click', 'Blip', 'Robot', 'Slam', 'La
 const scaler = (min1, max1, min2, max2, val) => {
     return (((val - min1) * (max2 - min2)) / (max1 - min1)) + min2
 }
-const soundPlay = (soundObj, multiplier, volume) => {
+const soundPlay = (soundObj, multiplier, volume, proportionality) => {
     const sound = new Howl({
         src: soundObj.src,
         sprite: soundObj.sprite,
-        volume: volume * multiplier
+        volume: volume * (proportionality === 'proportional' ? multiplier : .01)
     });
     sound.play(soundObj.spriteName);
 }

commit 18f1571b9c96d0f0d3cd3351f086e56677a8a761

fix bug introduced in commit ab67646 that broke volume slider

ocd-zen-garden/src/ControlBar.js
@@ -86,8 +86,8 @@ function ControlBar(props) {
     }
 
     const handleVolumeChange = evt => {
-        props.changeVolume(evt.target.id)
-        setVolume(evt.target.id)
+        props.changeVolume(evt.target.value)
+        setVolume(evt.target.value)
     }
     
     const handleProportionalVolumeChange = selection => {