9f281daf9a8164c9b305cdcaeb0693da2b312e65
Alex Rodgers <skyewulff@outlook.com>
2021-10-13 23:19:29 -0400

    create UserGarden component

---
+++ ocd-zen-garden/src/UserGarden.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Modal.css';
+
+import Garden from './Garden';
+
+function UserGarden() {
+    return (
+        <div>Test</div>
+    )
+}
+
+export default UserGarden;
