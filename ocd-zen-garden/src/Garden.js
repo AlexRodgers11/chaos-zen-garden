b579b3ee0657df228f2262112fab18120b962be7
Alex Rodgers <skyewulff@outlook.com>
2021-10-20 08:49:17 -0400

    remove remaining useState-based width, modal, and palette logic from Garden component; remove unused imports in same

--- ocd-zen-garden/src/Garden.js
+++ ocd-zen-garden/src/Garden.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
-import useToggle from './hooks/useToggle';
 import Header from './Header';
 import Modal from './Modal';
-import useCurrentWidth from './hooks/useCurrentWidth';
 import useCurrentHeight from './hooks/useCurrentHeight';
-import { palettes } from './utils';
 import Snake from './Snake';
 import Dots from './Dots';
 import BullsEye from './BullsEye';
@@ -34,19 +31,13 @@ import Asterisk from './Asterisk';
 
 
 function Garden(props) {
-    // let width = useCurrentWidth();
-    // const palette = useSelector((state) => state.palette.palette);
     const height = useCurrentHeight();
     const width = useSelector((state) => state.size.appWidth);
     const fullView = useSelector((state) => state.size.fullView);
     const volume = useSelector((state) => state.volume.volume);
     const modalContent = useSelector((state) => state.modalContent.modalContent);
-    const [colorPalette, setColorPalette] = useState(palettes[0]);
-    // const [volume, setVolume] = useState(65);
     const [numRings, setNumRings] = useState(10)
     const [bullsEyeShape, setBullsEyeShape] = useState('circle')
-    const [hideModal, toggleHideModal] = useToggle(false);
-    // const [modalContent, setModalContent] = useState('epilepsy-warning');
     const [organizationCount, setOrganizationCount] = useState(0);
     // const [resetTimer, toggleResetTimer] = useToggle(true);
 
@@ -68,11 +59,6 @@ function Garden(props) {
     //         alert('user may actually have OCD');
     //     }
     // }, [organizationCount])
-
-
-    const handleChangePalette = palette => {
-        setColorPalette(palette);
-    }
     
     const handleSetNumRings = num => {
         setNumRings(Number(num))
@@ -82,14 +68,8 @@ function Garden(props) {
         setBullsEyeShape(shape)
     }
 
-    // const handleSetModalContent = content => {
-    //     setModalContent(content);
-    //     toggleHideModal();
-    // }
-
     const displayFullSize = gardenPiece => {
         // let gardenPieceWidth = width > height ? height - 47 : width - 2;
-
         switch(gardenPiece) {
             case 'snake':
                 return <Snake className="Snake" />
@@ -144,18 +124,9 @@ function Garden(props) {
 
 
     if(!fullView) {
-        let gardenPieceWidth;
-        if(width >= 1000) {
-            gardenPieceWidth = width / 3;
-        } else if (width < 1000 && width > 600) {
-            gardenPieceWidth = width / 2;
-        } else {
-            gardenPieceWidth = width;
-        }
-
         return (
             <>
-            <Header changePalette={handleChangePalette} disableDropdowns={!modalContent} />
+            <Header />
             <div className="Garden">
                 <Snake className="Snake" />
                 <Dots className="Dots" />
