d475e4796d30f582f31c37c6c60f094059349008
Alex Rodgers <skyewulff@outlook.com>
2021-10-25 21:36:48 -0400

    add useGardenSpecs and usePieceSpecs hooks

---
+++ ocd-zen-garden/src/hooks/usePieceSpecs.js
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from 'react';
+import useToggle from './useToggle';
+import useSound from './useSound';
+
+const usePieceSpecs = (nextIdx, num, propVol, shp, sound, spd, txt) => {
+    const [isOrganized, toggleIsOrganized] = useToggle(false);
+    const [isOrganizing, toggleIsOrganizing] = useToggle(false);
+    const [nextIndex, setNextIndex] = useState(nextIdx);
+    const [number, setNumber] = useState(num);
+    const [proportionalVolume, setProportionalVolume] = useState(propVol);
+    const [shape, setShape] = useState(shp);
+    const [soundName, soundObj, setSound] = useSound(sound);
+    const [speed, setSpeed] = useState(spd);   
+    const [text, setText] = useState(txt);
+
+    return [
+        isOrganized, toggleIsOrganized, 
+        isOrganizing, toggleIsOrganizing, 
+        nextIndex, setNextIndex, 
+        number, setNumber,
+        speed, setSpeed, 
+        soundName, soundObj, setSound,
+        proportionalVolume, setProportionalVolume,
+        shape, setShape,
+        text, setText,
+    ];
+}
+
+export default usePieceSpecs;
