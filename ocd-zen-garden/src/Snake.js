5fcbcc9ab312e827b1b15c00e6804a4e1457e595
Alex Rodgers <skyewulff@outlook.com>
2021-10-26 21:21:45 -0400

    finish Snake refactoring with usePieceSpecs and useGardenSpecs

--- ocd-zen-garden/src/Snake.js
+++ ocd-zen-garden/src/Snake.js
@@ -8,34 +8,19 @@ import { getColor, scaler, soundPlay } from './utils';
 import ControlBar from './ControlBar';
 import { v4 as uuidv4 } from 'uuid';
 
-
 function Snake(props) {
-    const palette = useSelector((state) => state.palette.palette);
+    const gardenPalette = useSelector((state) => state.palette.palette);
     const [width, volume, fullView, dispatch] = useGardenSpecs();
-    const [
-        isOrganized, toggleIsOrganized, 
-        isOrganizing, toggleIsOrganizing, 
-        nextIndex, setNextIndex, 
-        numBoxes, setNumBoxes,
-        speed, setSpeed, 
-        soundName, soundObj, setSound,
-        proportionalVolume, setProportionalVolume,
-        shape, setShape,
-        text, setText,
-    ] = usePieceSpecs(0, 7, 'proportional', 'square', 'Slam', 1000, null);
-    // (nextIdx, num, propVol, shp, sound, spd, txt)
-
-    const [colorPalette, setColorPalette] = useState(palette);
-
+    const pieceSpecs = usePieceSpecs(0, props.number, props.proportionalVolume, props.shape, props.sound, props.speed, props.text);
+    const [piecePalette, setPiecePalette] = useState(gardenPalette);
 
     const createStartingBoxArray = num => {
         let boxes = [];
         for(let i = 1; i <= num; i++) {
-            let random = Math.random() * .45 * 100
             boxes.push({
                 id: i,
                 left: `${Math.random() * .40 + .05}` * (Math.random() > .5 ? 1 : -1),
-                color: getColor(i, colorPalette),
+                color: getColor(i, piecePalette),
                 key: uuidv4(),
             });
         }
@@ -45,56 +30,55 @@ function Snake(props) {
 
         }
         boxes[boxes.length - 1].volumeMultiplier = scaler(0, .9, .0035, .01, Math.abs(boxes[boxes.length - 1].left))
-
         return boxes;
     }
 
-    const [boxes, setBoxes] = useState(createStartingBoxArray(numBoxes));
+    const [boxes, setBoxes] = useState(createStartingBoxArray(pieceSpecs.number));
 
-    const firstUpdate = useRef(true);
+    const firstNextIndexUpdate = useRef(true);
     useEffect(()=>{
-        if(!firstUpdate.current) {
-            if(nextIndex < boxes.length){
+        if(!firstNextIndexUpdate.current) {
+            if(pieceSpecs.nextIndex < boxes.length){
                 setTimeout(()=>{
-                    organizeBoxes(nextIndex);
-                }, speed);
+                    organizeBoxes(pieceSpecs.nextIndex);
+                }, pieceSpecs.speed);
             }
         } else {
-            firstUpdate.current = false;
+            firstNextIndexUpdate.current = false;
         }     
-    }, [nextIndex]);
+    }, [pieceSpecs.nextIndex]);
 
     const colorFirstUpdate = useRef(true);
     useEffect(() => {
         if(!colorFirstUpdate.current) {
             let newBoxes = boxes.map(box => {
-                return {...box, color: getColor(box.id, palette)}
+                return {...box, color: getColor(box.id, gardenPalette)}
             });
-            setColorPalette(palette);
+            setPiecePalette(gardenPalette);
             setBoxes(newBoxes);
             colorsDoNotUpdate.current = true;
         } else {
             colorFirstUpdate.current = false;
         }
-    }, [palette]);
+    }, [gardenPalette]);
 
     const colorsDoNotUpdate = useRef(true)
     useEffect(() => {
         if(!colorsDoNotUpdate.current) {
             let newBoxes = boxes.map(box => {
-                return {...box, color: getColor(box.id, colorPalette)}
+                return {...box, color: getColor(box.id, piecePalette)}
             });
             setBoxes(newBoxes);
         } else {
             colorsDoNotUpdate.current = false;
         }
         
-    }, [colorPalette]);
+    }, [piecePalette]);
 
 
     const organizeBoxes = (idx) => {
         if(idx === 0) {
-            toggleIsOrganizing();
+            pieceSpecs.toggleIsOrganizing();
             dispatch(organizingCounterActions.incrementOrganizingCounter())
         }
         let newBoxes;
@@ -111,16 +95,16 @@ function Snake(props) {
                 }
             });
         }
-        soundPlay(soundObj, boxes[idx].volumeMultiplier, volume, proportionalVolume);
+        soundPlay(pieceSpecs.soundObj, boxes[idx].volumeMultiplier, volume, pieceSpecs.proportionalVolume);
         setBoxes(newBoxes);
         if(idx < boxes.length - 1) {
-            setNextIndex(idx + 1);
+            pieceSpecs.setNextIndex(idx + 1);
         } else {
             dispatch(organizingCounterActions.decrementOrganizingCounter());
             setTimeout(() => {
-                toggleIsOrganized();
-                toggleIsOrganizing();
-            }, speed);
+                pieceSpecs.toggleIsOrganized();
+                pieceSpecs.toggleIsOrganizing();
+            }, pieceSpecs.speed);
         }
     }
 
@@ -134,16 +118,16 @@ function Snake(props) {
         }
         boxes[boxes.length - 1].volumeMultiplier = scaler(0, .9, .35, 1, Math.abs(boxes[boxes.length - 1].left))
         setBoxes(newBoxes);
-        toggleIsOrganized();
+        pieceSpecs.toggleIsOrganized();
     }
 
-    const handleSetColorPalette = palette => {
-        colorsDoNotUpdate.current = false;
-        setColorPalette(palette);
-    }
+    // const handleSetColorPalette = palette => {
+        // colorsDoNotUpdate.current = false;
+    //     setColorPalette(palette);
+    // }
 
     const handleSetNumBoxes = num => {
-        setNumBoxes(Number(num));
+        pieceSpecs.setNumber(Number(num));
         setBoxes(createStartingBoxArray(Number(num)))
     }
 
@@ -152,12 +136,12 @@ function Snake(props) {
             [
                 props.id, {
                     type: 'snake',
-                    palette: palette,
-                    speed: speed,
-                    sound: soundName,
-                    proportionalVolume: proportionalVolume,
-                    number: numBoxes,
-                    shape: shape ,
+                    palette: piecePalette,
+                    speed: pieceSpecs.speed,
+                    sound: pieceSpecs.soundName,
+                    proportionalVolume: pieceSpecs.proportionalVolume,
+                    number: pieceSpecs.number,
+                    shape: pieceSpecs.shape ,
                     text: null
                 }
             ]
@@ -165,16 +149,16 @@ function Snake(props) {
     }
 
     return (
-        <div style={{margin: fullView ? '0 auto' : 0, width: `${width}px`, height: `${width}px`, border: '1px solid black', backgroundColor: getColor('base', colorPalette)}}>
+        <div style={{margin: fullView ? '0 auto' : 0, width: `${width}px`, height: `${width}px`, border: '1px solid black', backgroundColor: getColor('base', piecePalette)}}>
             <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%', width: '100%'}}>
                 <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%'}}>
                         {boxes.map(box => {
                             return (
-                                <div key={box.key} style={{position: 'relative', boxSizing: 'border-box', border: `1px solid ${getColor('border', colorPalette)}`, width: `${Math.floor(width * .75 / numBoxes)}px`, height: `${Math.floor(width * .75 / numBoxes)}px`, padding: 0, marginTop: '0', marginBottom: '0', left: `${box.left * width * .75 / numBoxes}px`, backgroundColor: `${box.color}`, borderRadius: `${shape === 'circle' ? '50%' : 0}`}}></div>
+                                <div key={box.key} style={{position: 'relative', boxSizing: 'border-box', border: `1px solid ${getColor('border', piecePalette)}`, width: `${Math.floor(width * .75 / pieceSpecs.number)}px`, height: `${Math.floor(width * .75 / pieceSpecs.number)}px`, padding: 0, marginTop: '0', marginBottom: '0', left: `${box.left * width * .75 / pieceSpecs.number}px`, backgroundColor: `${box.color}`, borderRadius: `${pieceSpecs.shape === 'circle' ? '50%' : 0}`}}></div>
                             )
                         })}
                 </div>
-                <ControlBar id={props.id} toggleFullView={handleToggleFullView} shape={shape} shapes={['circle', 'square']} changeShape={setShape} changeProportionalVolume={setProportionalVolume} proportionalVolume={proportionalVolume} palette={colorPalette} setPalette={handleSetColorPalette} minNum={4} maxNum={30} number={numBoxes} setNumber={handleSetNumBoxes} isOrganizing={isOrganizing} isOrganized={isOrganized} setSpeed={setSpeed} setSound={setSound} soundValue='Slam' organizedFunction={scatterBoxes} unorganizedFunction={() => organizeBoxes(0)} unorgButton='Scatter' orgButton='Organize' />
+                <ControlBar id={props.id} toggleFullView={handleToggleFullView} shape={pieceSpecs.shape} shapes={['circle', 'square']} changeShape={pieceSpecs.setShape} changeProportionalVolume={pieceSpecs.setProportionalVolume} proportionalVolume={pieceSpecs.proportionalVolume} palette={piecePalette} setPalette={setPiecePalette} minNum={4} maxNum={30} number={pieceSpecs.number} setNumber={handleSetNumBoxes} isOrganizing={pieceSpecs.isOrganizing} isOrganized={pieceSpecs.isOrganized} setSpeed={pieceSpecs.setSpeed} setSound={pieceSpecs.setSound} soundValue='Slam' organizedFunction={scatterBoxes} unorganizedFunction={() => organizeBoxes(0)} unorgButton='Scatter' orgButton='Organize' />
             </div>
         </div>
     )
