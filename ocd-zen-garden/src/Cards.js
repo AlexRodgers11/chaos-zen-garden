1d8187b6d61d374d991567239b05aa3797231346
Alex Rodgers <skyewulff@outlook.com>
2021-10-26 22:38:18 -0400

    refactor Cards to use the usePieceSpecs and useGardenSpecs hooks

--- ocd-zen-garden/src/Cards.js
+++ ocd-zen-garden/src/Cards.js
@@ -1,26 +1,18 @@
 import React, {useEffect, useRef, useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import useGardenSpecs from './hooks/useGardenSpecs';
+import usePieceSpecs from './hooks/usePieceSpecs';
 import { sizeActions } from './store/size';
 import { organizingCounterActions } from './store/organizing-counter';
-import useToggle from './hooks/useToggle';
 import { v4 as uuidv4 } from 'uuid';
-import { getColor, getSound, scaler, soundPlay } from './utils';
+import { getColor, scaler, soundPlay } from './utils';
 import ControlBar from './ControlBar';
 
 function Cards(props) {
-    const width = useSelector((state) => state.size.pieceWidth);
     const palette = useSelector((state) => state.palette.palette);
-    const volume = useSelector((state) => state.volume.volume);
-    const fullView = useSelector((state) => state.size.fullView);
-    const [isOrganized, toggleIsOrganized] = useToggle(false);
-    const [isOrganizing, toggleIsOrganizing] = useToggle(false);
-    const [numCards, setNumCards] = useState(5);
-    const [nextIndex, setNextIndex] = useState(0);
     const [colorPalette, setColorPalette] = useState(palette);
-    const [speed, setSpeed] = useState(1000);
-    const [sound, setSound] = useState(getSound('Whoosh'));
-    const [proportionalVolume, setProportionalVolume] = useState('proportional');
-    const dispatch = useDispatch();
+    const [width, volume, fullView, dispatch] = useGardenSpecs();
+    const pieceSpecs = usePieceSpecs(0, props.number, props.proportionalVolume, props.shape, props.sound, props.speed, props.text);
 
     const createStartingCardsArray = num => {
         let cards = [];
@@ -38,23 +30,22 @@ function Cards(props) {
         
     }
 
-
-    const [cards, setCards] = useState(createStartingCardsArray(numCards));
+    const [cards, setCards] = useState(createStartingCardsArray(pieceSpecs.number));
 
     const firstUpdate = useRef(true);
     useEffect(() => {
         if(!firstUpdate.current) {
-            if(nextIndex < cards.length){
+            if(pieceSpecs.nextIndex < cards.length){
                 setTimeout(() => {
-                    organize(nextIndex);
-                }, speed);
+                    organize(pieceSpecs.nextIndex);
+                }, pieceSpecs.speed);
             } else {
-                toggleIsOrganizing();
-                toggleIsOrganized();
+                pieceSpecs.toggleIsOrganizing();
+                pieceSpecs.toggleIsOrganized();
                 dispatch(organizingCounterActions.decrementOrganizingCounter());
             }
         } else {firstUpdate.current = false}
-    }, [nextIndex])
+    }, [pieceSpecs.nextIndex])
     
     const colorsFirstUpdate = useRef(true)
     useEffect(() => {
@@ -86,7 +77,7 @@ function Cards(props) {
 
     const organize = (idx) => {
         if(idx === 0) {
-            toggleIsOrganizing();
+            pieceSpecs.toggleIsOrganizing();
             dispatch(organizingCounterActions.incrementOrganizingCounter()) 
         }
         let newCards = cards.map(card => {
@@ -96,14 +87,14 @@ function Cards(props) {
                 return card;
             }
         });
-        soundPlay(sound, cards[idx].volumeMultiplier, volume, proportionalVolume);
+        soundPlay(pieceSpecs.soundObj, cards[idx].volumeMultiplier, volume, pieceSpecs.proportionalVolume);
         setCards(newCards);
-        setNextIndex(idx + 1)        
+        pieceSpecs.setNextIndex(idx + 1)        
     }
 
     const scatter = () => {
         let newCards = cards.map(card => {
-            let random = (5 + Math.random() * 15) * (card.id <= numCards ? 1 : -1)
+            let random = (5 + Math.random() * 15) * (card.id <= pieceSpecs.number ? 1 : -1)
             return {
                 ...card, 
                 offset: random,
@@ -111,36 +102,25 @@ function Cards(props) {
             }
         })
         setCards(newCards);
-        toggleIsOrganized();
+        pieceSpecs.toggleIsOrganized();
     }
 
-    const handleSetSpeed = time => {
-        setSpeed(time);
-    }
-
-    const handleSetSound = sound => {
-        setSound(getSound(sound));
-    }
     const handleSetNumCards = num => {
-        setNumCards(Number(num));
+        pieceSpecs.setNumber(Number(num));
         setCards(createStartingCardsArray(Number(num)))
     }
 
     const handleSetColorPalette = palette => {
         colorsDoNotUpdate.current = false;
         setColorPalette(palette);
-    }
-
-    const handleChangeProportionalVolume = selection => {
-        setProportionalVolume(selection);
-    }
+    } 
 
     const displayCards = () => {
         let cardLines = []
         let newLine = []
         for(let k = 0; k < cards.length; k++){
             newLine.push(cards[k]);
-            if(newLine.length === numCards){
+            if(newLine.length === pieceSpecs.number){
                 cardLines.push(newLine);
                 newLine = []
             }
@@ -154,12 +134,12 @@ function Cards(props) {
                 props.id, {
                     type: 'cards',
                     palette: palette,
-                    speed: speed,
-                    sound: sound,
-                    proportionalVolume: proportionalVolume,
-                    number: numCards,
-                    shape: null ,
-                    text: null
+                    speed: pieceSpecs.speed,
+                    sound: pieceSpecs.soundName,
+                    proportionalVolume:pieceSpecs. proportionalVolume,
+                    number: pieceSpecs.number,
+                    shape: pieceSpecs.shape ,
+                    text: pieceSpecs.text
                 }
             ]
         ));
@@ -171,12 +151,12 @@ function Cards(props) {
                 <div style={{position: 'relative', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '100%', height: '100%'}}>
                         {displayCards().map(cardLine => {
                             let lineKey = uuidv4()
-                            return <div key={lineKey} style={{position: 'relative', margin: '7% 0', paddingLeft: `${((cards.length / 2) - 1) * (.8 * width * .70 * (1 / (numCards + 2)))}px`}}>{cardLine.map(card => {
-                                return <div key={card.key} style={{position: 'relative', zIndex: card.id,  right: `${card.id - (cards.length / 2) <= 0 ? (card.id - 1) * (.8 * width * .70 * (1 / (numCards + 2))) : (card.id - (cards.length / 2) - 1) * (.8 * width * .70 * (1 / (numCards + 2)))}px`, display: 'inline-block', backgroundColor:`${card.color}`, border: `1px solid ${getColor('border', colorPalette)}`, width: `${width * .70 * (1 / (numCards + 2))}px`, height: `${1.5 * width * .70 * (1 / (numCards + 2))}px`, top: card.offset}}></div>
+                            return <div key={lineKey} style={{position: 'relative', margin: '7% 0', paddingLeft: `${((cards.length / 2) - 1) * (.8 * width * .70 * (1 / (pieceSpecs.number + 2)))}px`}}>{cardLine.map(card => {
+                                return <div key={card.key} style={{position: 'relative', zIndex: card.id,  right: `${card.id - (cards.length / 2) <= 0 ? (card.id - 1) * (.8 * width * .70 * (1 / (pieceSpecs.number + 2))) : (card.id - (cards.length / 2) - 1) * (.8 * width * .70 * (1 / (pieceSpecs.number + 2)))}px`, display: 'inline-block', backgroundColor:`${card.color}`, border: `1px solid ${getColor('border', colorPalette)}`, width: `${width * .70 * (1 / (pieceSpecs.number + 2))}px`, height: `${1.5 * width * .70 * (1 / (pieceSpecs.number + 2))}px`, top: card.offset}}></div>
                             })}</div>
                         })}
                 </div>
-                <ControlBar id={props.id} toggleFullView={handleToggleFullView} changeProportionalVolume={handleChangeProportionalVolume} proportionalVolume={proportionalVolume} palette={colorPalette} setPalette={handleSetColorPalette} minNum={3} maxNum={30} number={numCards} setNumber={handleSetNumCards} isOrganizing={isOrganizing} isOrganized={isOrganized} setSpeed={handleSetSpeed} setSound={handleSetSound} soundValue='Ding' organizedFunction={scatter} unorganizedFunction={() => organize(0)} unorgButton='Scatter' orgButton='Organize'/>
+                <ControlBar id={props.id} toggleFullView={handleToggleFullView} changeProportionalVolume={pieceSpecs.setProportionalVolume} proportionalVolume={pieceSpecs.proportionalVolume} palette={colorPalette} setPalette={handleSetColorPalette} minNum={3} maxNum={30} number={pieceSpecs.number} setNumber={handleSetNumCards} isOrganizing={pieceSpecs.isOrganizing} isOrganized={pieceSpecs.isOrganized} setSpeed={pieceSpecs.setSpeed} setSound={pieceSpecs.setSound} soundValue='Ding' organizedFunction={scatter} unorganizedFunction={() => organize(0)} unorgButton='Scatter' orgButton='Organize'/>
             </div>
         </div>
     )
